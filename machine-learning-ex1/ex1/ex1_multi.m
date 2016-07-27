fd0f58f41f9939086cc1b84f70c7776354981033
Augusto Lopez Dantas <aldantas@protonmail.com>
2016-07-26 23:49:16 -0300

    Week 2 - Programmin Assignment Optionals

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -60,13 +60,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -82,16 +82,16 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.03;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:numel(J_history), J_history, 'b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,9 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
-
+X = ([1650 3] - mu) ./ sigma;
+X = [1 X];
+price = X * theta; % You should change this
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
@@ -120,12 +120,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -149,8 +149,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+X = [1 1650 3];
+price = X * theta; % You should change this
 
 % ============================================================
 
